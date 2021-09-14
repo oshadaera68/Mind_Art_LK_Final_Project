af1c83c18971d1cb1e44e03ad35cca59f62f2eeb
oshadaera68 <mindartlk010@gmail.com>
2021-09-14 21:59:55 +0530

    added files in supplier order form

--- src/controller/SupplierOrderFormController.java
+++ src/controller/SupplierOrderFormController.java
@@ -1,4 +1,17 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class SupplierOrderFormController {
+    public void addOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void searchOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void selectAllOrderOnAction(ActionEvent actionEvent) {
+    }
 }
