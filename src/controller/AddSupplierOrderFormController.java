3c22cf4c4528a92451a9a1ceb3f2c5c852061e09
oshadaera68 <mindartlk010@gmail.com>
2021-09-14 22:27:10 +0530

    modified files

--- src/controller/AddSupplierOrderFormController.java
+++ src/controller/AddSupplierOrderFormController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class AddSupplierOrderFormController {
+    public JFXTextField txtID;
+    public JFXTextField txtName;
+    public JFXTextField txtDate;
+
+    public void addSupplierOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void supplierOnAction(ActionEvent actionEvent) {
+    }
 }
