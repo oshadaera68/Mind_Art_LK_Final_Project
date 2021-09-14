128b674d51a22b1db8250d1fa1bf9f98b38497d8
oshadaera68 <mindartlk010@gmail.com>
2021-09-14 22:48:25 +0530

    modified UI for navigation

--- src/controller/SupplierOrderFormController.java
+++ src/controller/SupplierOrderFormController.java
@@ -1,8 +1,18 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class SupplierOrderFormController {
+    public AnchorPane supplierContexts;
+
     public void addSupplierOrderOnAction(ActionEvent actionEvent) {
 
     }
@@ -17,5 +27,14 @@ public class SupplierOrderFormController {
 
     public void selectAllSupplierOrderOnAction(ActionEvent actionEvent) {
 
+    }
+
+    public void supplierOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../view/SupplierForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Stage window = (Stage) supplierContexts.getScene().getWindow();
+        window.setScene(new Scene(load));
+        window.setTitle("Supplier Form");
+        window.show();
     }
 }
