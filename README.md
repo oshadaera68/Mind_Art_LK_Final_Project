8ab96460ca26e84088abca5b4a91767a295e91cf
oshadaera68 <mindartlk010@gmail.com>
2021-09-15 13:17:58 +0530

    new files

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Mind_Art_LK_Final_Project_1
 
-/*
-*hosted by - oshada eranga@Mind Art LK
-*created project - 2021/09/12
-*Using Softwares - Intelij Idea 2021.2.1,MySQL Community Server 8.0.25,SceneBulider 8.5.0
-*/
+
+hosted by - oshada eranga@Mind Art LK
+created project - 2021/09/12
+Using Softwares - Intelij Idea 2021.2.1(IDE),
+MySQL Community Server 8.0.25(Database),
+SceneBuilder 8.5.0(JavaFx)
+
--- src/controller/SupplierDetailFormController.java
+++ src/controller/SupplierDetailFormController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class SupplierDetailFormController {
+    public void addSupplierDetailOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void searchSupplierDetailOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void selectAllFormOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void deleteSupplierDetailsOnAction(ActionEvent actionEvent) {
+
+    }
 }
