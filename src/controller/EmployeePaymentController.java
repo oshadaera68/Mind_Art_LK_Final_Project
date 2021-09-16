3d1ebe9df6941492cfbb65c596174991b1ce1388
oshadaera68 <mindartlk010@gmail.com>
2021-09-16 06:58:25 +0530

    edited

--- src/controller/EmployeePaymentController.java
+++ src/controller/EmployeePaymentController.java
@@ -3,9 +3,16 @@ package controller;
 import com.jfoenix.controls.JFXTextField;
 import db.DbConnection;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import model.EmployeePayment;
 
+import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -15,6 +22,7 @@ public class EmployeePaymentController {
     public JFXTextField txtID;
     public JFXTextField txtDate;
     public JFXTextField txtAmount;
+    public AnchorPane context;
 
     public void addPaymentOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
         EmployeePayment c1 = new EmployeePayment(
@@ -37,5 +45,14 @@ public class EmployeePaymentController {
         stm.setObject(3,e.getAmount());
 
         return stm.executeUpdate()>0;
+    }
+
+    public void backWardOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../view/EmployeeForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Stage window = (Stage) context.getScene().getWindow();
+        window.setScene(new Scene(load));
+        window.setTitle("Employee Form");
+        window.show();
     }
 }
--- src/model/EmployeePayment.java
+++ src/model/EmployeePayment.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.Date;
-
 public class EmployeePayment {
     private String Id;
     private String Date;
