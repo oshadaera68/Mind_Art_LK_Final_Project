b233f7fe33ac0cb55cd7a081e422a2bae1506a42
oshadaera68 <mindartlk010@gmail.com>
2021-09-20 07:10:44 +0530

    added files and modified

--- src/controller/ExpenseFormController.java
+++ src/controller/ExpenseFormController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class ExpenseFormController {
+    public void addExpenseOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void searchExpenseOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void deleteExpenseOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void selectAllExpenseOnAction(ActionEvent actionEvent) {
+
+    }
 }
